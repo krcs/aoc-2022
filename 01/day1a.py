ba9e3e937cacf56568c37e624946d9dcd5e217a9
unknown <k.netpost@gmail.com>
2022-12-01 07:56:03 +0100

    python - done

--- 01/day1a.py
+++ 01/day1a.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python3
 input = "./input.txt"
 
+def read_lines(file):
+    result = []
+
+    with open(input,'r') as f:
+        while True:
+            line = f.readline()
+
+            if len(line)>0:
+                result.append(line.strip())
+
+            if not line:
+                break
+    return result
+
 elfs = []
 calories = []
 
-with open(input,'r') as f:
-    while True:
-        line = f.read().strip()
+lines = read_lines(input)
 
-        if not line:
-            break
+for line in lines:
+    if len(line)==0:
+        elfs.append(sum(calories))
+        calories = []
+        continue
 
-        if line!="\n":
-            print(line,len(line))
-            #calories.append(int(line))
-        #elif len(line)==0:
-        #    elfs.append(sum(calories))
-       #     calories = []
+    calories.append(int(line))
 
-
-#print(max(elfs))
+print(max(elfs))
--- 01/day1b.py
+++ 01/day1b.py
@@ -1,21 +1,31 @@
 #!/usr/bin/python3
+input = "./input.txt"
+
+def read_lines(file):
+    result = []
+
+    with open(input,'r') as f:
+        while True:
+            line = f.readline()
+
+            if len(line)>0:
+                result.append(line.strip())
+
+            if not line:
+                break
+    return result
+
 elfs = []
 calories = []
 
-f = open("./input.txt","r")
-while True:
-    line = f.readline()
+lines = read_lines(input)
 
-    if not line:
-        break
-
-    if line == '\n':
+for line in lines:
+    if len(line)==0:
         elfs.append(sum(calories))
         calories = []
         continue
 
     calories.append(int(line))
 
-f.close()
-s=sorted(elfs,reverse=True)
-print(sum([s[0],s[1],s[2]]))
+print(sum(sorted(elfs)[-3:]))
