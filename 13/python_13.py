9fad0509d95148c46246042668329601e0289799
unknown <k.netpost@gmail.com>
2022-12-13 15:35:09 +0100

    13 - python, part 1 unfinished

--- 13/python_13.py
+++ 13/python_13.py
@@ -22,8 +22,55 @@ def read_lines(file):
 
 lines = read_lines(input)
 
+def compare(left, right):
+    if type(left)==list and type(right)==list:
+        if len(left) == 0 and len(right) > 0:
+            return True
+        elif len(left) > 0 and len(right) == 0:
+            return False
+
+    elif type(left)==int and type(right)==int:
+        if left != right:
+            return left < right
+        else:
+            return None
+
+    elif type(left)==int and type(right)==list:
+        return compare([left], right)
+
+    elif type(left)==list and type(right)==int:
+        return compare(left, [right])
+
+    n = len(left) if len(left) < len(right) else len(right)
+
+    result = None
+
+    for idx in range(n):
+       result = compare(left[idx], right[idx])
+       if result != None:
+           break
+
+    #if result == None:
+    #    print(result, len(left),len(right))
+
+    return result
+
+pairs = []
+pair = []
+
 for line in lines:
     if not line:
-        continue
-    print(eval(line))
-    
+        pairs.append(pair)
+        pair = []
+    else: 
+        pair.append(eval(line))
+
+pairs.append(pair)
+
+for idx, pair in enumerate(pairs):
+    c = compare(pair[0], pair[1])
+    print(f"Pair [{idx}]:  {pair} - {c}")
+
+#pair = pairs[3]
+#print(f"Result: {compare(pair[0], pair[1])}")
+#
